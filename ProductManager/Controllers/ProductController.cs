51f73c8b25cf06d263b7d917b40f70ca3a12fdf5
Litt1eRebe1 <tilaniebresler@gmail.com>
2022-05-31 21:53:01 +0200

    Product and Queue Controlled additions

--- ProductManager/Controllers/ProductController.cs
+++ ProductManager/Controllers/ProductController.cs
@@ -9,7 +9,53 @@ namespace ProductManager.Controllers
     [Route("[controller]")]
     public class ProductController : ControllerBase
     {
-       
-       
+        private IProductLogic _productLogic;
+
+        public ProductController(IProductLogic productLogic)
+        {
+            _productLogic = productLogic;
+        }
+
+        [Route("Health")]
+        [HttpGet]
+        public async Task<string> Health()
+        {
+            return "The Product Service is Healthy :)";
+        }
+
+        [Route("List")]
+        [HttpGet]
+        public async Task<IEnumerable<Product>> List()
+        {
+            return await _productLogic.List();
+        }
+
+        [Route("Get/{id}")]
+        [HttpGet]
+        public async Task<Product> Get(string id)
+        {
+            return await _productLogic.Get(id);
+        }
+
+        [Route("Add")]
+        [HttpPost]
+        public async Task<bool> Add([FromBody] Product product)
+        {
+            return await _productLogic.Add(product);
+        }
+
+        [Route("Update/{id}")]
+        [HttpPost]
+        public async Task<bool> Update([FromBody] Product product, string id)
+        {
+            return await _productLogic.Update(product, id);
+        }
+
+        [Route("Delete/{id}")]
+        [HttpDelete]
+        public async Task<bool> Delete(string id)
+        {
+            return await _productLogic.Delete(id);
+        }
     }
 }
--- ProductManager/Controllers/QueueController.cs
+++ ProductManager/Controllers/QueueController.cs
@@ -8,6 +8,53 @@ namespace ProductManager.Controllers
     [Route("[controller]")]
     public class QueueController : ControllerBase
     {
-      
+        private IQueueLogic _queueLogic;
+
+        public QueueController(IQueueLogic queueLogic)
+        {
+            _queueLogic = queueLogic;
+        }
+
+        [Route("Health")]
+        [HttpGet]
+        public async Task<string> Health()
+        {
+            return "The Queue Service is Healthy :)";
+        }
+
+        [Route("List")]
+        [HttpGet]
+        public async Task<IEnumerable<QueueModel>> List()
+        {
+            return await _queueLogic.List();
+        }
+
+        [Route("Get/{id}")]
+        [HttpGet]
+        public async Task<QueueModel> Get(string id)
+        {
+            return await _queueLogic.Get(id);
+        }
+
+        [Route("Add")]
+        [HttpPost]
+        public async Task<bool> Add(QueueModel queue)
+        {
+            return await _queueLogic.Add(queue);
+        }
+
+        [Route("Update/{id}")]
+        [HttpPost]
+        public async Task<bool> Update(QueueModel queue, string id)
+        {
+            return await _queueLogic.Update(queue, id);
+        }
+
+        [Route("Delete/{id}")]
+        [HttpDelete]
+        public async Task<bool> Delete(string id)
+        {
+            return await _queueLogic.Delete(id);
+        }
     }
 }
