108f18403455f614d60d12cc6760cb078fb2cd49
Litt1eRebe1 <tilaniebresler@gmail.com>
2022-05-31 22:01:45 +0200

    Task and User Controller addition

--- ProductManager/Controllers/TaskController.cs
+++ ProductManager/Controllers/TaskController.cs
@@ -9,6 +9,60 @@ namespace ProductManager.Controllers
     [Route("[controller]")]
     public class TaskController : ControllerBase
     {
-        
+        private readonly ITaskLogic _taskLogic;
+
+        public TaskController(ITaskLogic taskLogic)
+        {
+            _taskLogic = taskLogic;
+        }
+
+        [Route("Health")]
+        [HttpGet]
+        public async Task<string> Health()
+        {
+            return "The Task Service is Healthy :)";
+        }
+
+        [Route("Enqueue/{queueId}/{taskId}")]
+        [HttpGet]
+        public async Task<bool> Enqueue(string queueId, string taskId)
+        {
+            return await _taskLogic.Enqueue(queueId, taskId);
+        }
+
+        [Route("Dequeue/{queueId}/{taskId}")]
+        [HttpGet]
+        public async Task<bool> Dequeue(string queueId, string taskId)
+        {
+            return await _taskLogic.Dequeue(queueId, taskId);
+        }
+
+        [Route("Complete/{queueId}/{taskId}")]
+        [HttpGet]
+        public async Task<bool> Complete(string queueId, string taskId)
+        {
+            return await _taskLogic.Complete(queueId, taskId);
+        }
+
+        [Route("List")]
+        [HttpGet]
+        public async Task<IEnumerable<TaskModel>> List()
+        {
+            return await _taskLogic.List();
+        }
+
+        [Route("Get/{id}")]
+        [HttpGet]
+        public async Task<TaskModel> Get(string id)
+        {
+            return await _taskLogic.Get(id);
+        }
+
+        [Route("Add")]
+        [HttpPost]
+        public async Task<bool> Add([FromBody] TaskModel task)
+        {
+            return await _taskLogic.Add(task);
+        }
     }
 }
--- ProductManager/Controllers/UserController.cs
+++ ProductManager/Controllers/UserController.cs
@@ -8,6 +8,66 @@ namespace ProductManager.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        
+        private IUserLogic _userLogic;
+        public UserController(IUserLogic userLogic)
+        {
+            _userLogic = userLogic;
+        }
+
+        [Route("Health")]
+        [HttpGet]
+        public async Task<string> Health()
+        {
+            return "The User Service is Healthy :)";
+        }
+
+        [Route("List")]
+        [HttpGet]
+        public async Task<IEnumerable<User>> List()
+        {
+            return await _userLogic.List();
+        }
+
+        [Route("Get/{id}")]
+        [HttpGet]
+        public async Task<User> Get(string id)
+        {
+            return await _userLogic.Get(id);
+        }
+
+        [Route("Add")]
+        [HttpPost]
+        public async Task<bool> Add(User user)
+        {
+            return await _userLogic.Add(user);
+        }
+
+        [Route("Update/{id}")]
+        [HttpPost]
+        public async Task<bool> Update(User user, string id)
+        {
+            return await _userLogic.Update(user, id);
+        }
+
+        [Route("Delete/{id}")]
+        [HttpDelete]
+        public async Task<bool> Delete(string id)
+        {
+            return await _userLogic.Delete(id);
+        }
+
+        [Route("Enqueue/{queueId}/{userId}")]
+        [HttpGet]
+        public async Task<bool> Enqueue(string queueId, string userId)
+        {
+            return await _userLogic.Enqueue(queueId, userId);
+        }
+
+        [Route("Dequeue/{queueId}/{userId}")]
+        [HttpGet]
+        public async Task<bool> Dequeue(string queueId, string userId)
+        {
+            return await _userLogic.Dequeue(queueId, userId);
+        }
     }
 }
