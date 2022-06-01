b7f6d9c25d8d83d0416e7a8d95fa19cee2a5bf7f
Litt1eRebe1 <tilaniebresler@gmail.com>
2022-06-01 18:33:04 +0200

    Db Service implementation

--- ProductManager/Services/CosmosDbService.cs
+++ ProductManager/Services/CosmosDbService.cs
@@ -19,27 +19,44 @@ namespace ProductManager.Services
 
         public async Task AddItemAsync<T>(T item, DataLayerType type)
         {
-          
+            await this._container.CreateItemAsync<T>(item, new PartitionKey((int)type));
         }
 
         public async Task DeleteItemAsync<T>(string id, DataLayerType type)
         {
-            
+            await this._container.DeleteItemAsync<T>(id, new PartitionKey((int)type));
         }
 
         public async Task<T> GetItemAsync<T>(string id, DataLayerType type)
         {
-            return default(T);
+            try
+            {
+                ItemResponse<T> response = await this._container.ReadItemAsync<T>(id, new PartitionKey((int)type));
+                return response.Resource;
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return default(T);
+            }
         }
 
         public async Task<IEnumerable<T>> GetItemsAsync<T>(DataLayerType type)
         {
-            return null;
+            var query = this._container.GetItemQueryIterator<T>(new QueryDefinition($"SELECT * FROM c WHERE c.DataLayerType = {(int)type}"));
+            List<T> results = new List<T>();
+            while (query.HasMoreResults)
+            {
+                var response = await query.ReadNextAsync();
+
+                results.AddRange(response.ToList());
+            }
+
+            return results;
         }
 
         public async Task UpdateItemAsync<T>(string id, T item)
         {
-           
+            await this._container.ReplaceItemAsync<T>(item, id);
         }
     }
 }
